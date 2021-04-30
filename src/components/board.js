aec4a0f54ed23369dcb42de8576c0858ccdd15e8
Christopher Wilson <chrisgwilson678@gmail.com>
2021-04-29 22:39:38 -0400

    refactor board component to map squares

--- src/components/board.js
+++ src/components/board.js
@@ -3,38 +3,12 @@ import './square';
 import '../index.css';
 import Square from './square';
 
-const Board = ({ squares, onClick }) => {
-    
-     const renderSquare = (i) => {
-      return( <Square 
-                value={squares[i]}
-                onClick={() => onClick(i)} 
-              />
-            );
-     }
-  
-      return (
-        <div>
-          <div className="board-row">
-            {renderSquare(0)}
-            {renderSquare(1)}
-            {renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {renderSquare(3)}
-            {renderSquare(4)}
-            {renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {renderSquare(6)}
-            {renderSquare(7)}
-            {renderSquare(8)}
-          </div>
-        </div>
-      );
-}
-
-
-
+const Board = ({ squares, onClick }) => (
+    <div className="board">
+        {squares.map((square, i) => ( 
+            <Square key={i} value={square} onClick={() => onClick(i)}/>
+        ))}
+    </div>
+);
 
 export default Board;
